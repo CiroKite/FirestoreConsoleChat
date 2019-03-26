5f695afa6917ec72e9362508f0ea86bedc27d264
Ciro <ciro@qualfarmacia.com.br>
2019-03-26 10:32:58 -0300

    - Added run.js - Added package.json and package-lock.json - Added .gitignore

---
+++ run.js
@@ -0,0 +1,39 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+let users = [process.argv[2], process.argv[3]];
+
+users = users.sort((a, b) => a.localeCompare(b));
+
+let messages = db.collection('chats').doc(users[0] + '.' + users[1]).collection('messages');
+
+let observer = messages.orderBy('timestamp').onSnapshot(docSnapshot => {
+	docSnapshot.docChanges().forEach((change) => {
+		if (change.type === 'added') {
+			console.log(change.doc.data().from, ':', change.doc.data().message);
+		}
+		if (change.type === 'removed') {
+			console.log('removed', ':', change.doc.data().message);
+		}
+	})
+}, err => {
+	console.log(`Encountered error: ${err}`);
+});
+
+process.stdin.on('data', input => {
+	let texto = input.toString();
+
+	messages.add({
+		from: process.argv[2],
+		timestamp: new Date().getTime(),
+		message: texto
+	}).then().catch(console.log);
+});
+
